e8079fd5c26617db6412bb302a061642041e1af0
lf- <lf-@users.noreply.github.com>
2019-04-23 01:13:39 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ShortcutItPy
+
+This small add-in for Fusion 360 adds icons to some commands and puts them in
+a panel so you can add shortcuts for them.
+
+Currently the commands include:
+- Isolate
+- Unisolate
+- Unisolate All
+- Find in Browser (but it refuses to be added to the panel, can't tell why)
+
+If you want me to add another, please file an issue on this repository or make
+a pull request to add it.
+
+The iconography used is Adwaita from the GNOME project.
---
+++ ShortcutItPy.py
@@ -0,0 +1,50 @@
+#Author-lf
+#Description-Add a bunch of commands to a toolbar panel in Tools so they can be shortcut
+
+import adsk.core, adsk.fusion, adsk.cam, traceback
+
+def run(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        
+        ws = ui.workspaces.itemById('FusionSolidEnvironment')
+        panels = ws.toolbarPanels
+        myPanel = panels.itemById('ShortcutItPanel')
+        if myPanel:
+            myPanel.deleteMe()
+        
+        myPanel = panels.add('ShortcutItPanel', 'Shortcut It', '')
+        needCmdDefs = (
+            ('IsolateCmd', './isolate'),
+            ('UnisolateCmd', './unisolate'),
+            ('UnisolateAllCmd', './unisolate'),
+            ('FindInBrowser', './findinbrowser'),
+        )
+        for cmdName, resDir in needCmdDefs:
+            cmd = ui.commandDefinitions.itemById(cmdName)
+            if not cmd:
+                ui.messageBox('Cannot find {}'.format(cmdName))
+                continue
+            cmd.resourceFolder = resDir
+            myPanel.controls.addCommand(cmd)
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+def stop(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        ws = ui.workspaces.itemById('FusionSolidEnvironment')
+        panels = ws.toolbarPanels
+        myPanel = panels.itemById('ShortcutItPanel')
+        if myPanel:
+            myPanel.deleteMe()
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
